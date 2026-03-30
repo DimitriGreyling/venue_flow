94c2711133442119d3ab50007f9cc47bea38e583
Dimitri Greyling <dimitri.greyling@toppangravity.com>
2026-03-30 09:35:53 +0200

    changes for base project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+   return Consumer(builder: (context, ref, child) {
+     return MaterialApp.router(
+      
+     );
+   },);
   }
 }
