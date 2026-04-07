43c2a7bb8eded3562eef294f6a75009a1f4ce90b
Dimitri Greyling <dimitri.greyling@toppangravity.com>
2026-04-07 12:32:57 +0200

    implement table constants

---
+++ lib/constants/supabase_table_names.dart
@@ -0,0 +1,4 @@
+class SupabaseTableNames {
+ static const String formTable = 'forms';
+ static const String formSubmissionTable = 'form_submissions';
+}
--- lib/repositories/form_repository.dart
+++ lib/repositories/form_repository.dart
@@ -1,25 +1,29 @@
 import 'dart:developer';
 
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:venue_flow_app/constants/supabase_table_names.dart';
 import 'package:venue_flow_app/models/dynamic_form_model.dart';
 
 class FormRepository {
-  final SupabaseClient _client;
-
+  
+  //CONSTRUCTOR
   FormRepository({
     required SupabaseClient client,
   }) : _client = client;
 
+  //VARIABLES
+  final SupabaseClient _client;
+  final _tableName = SupabaseTableNames.formTable;
+
   Future<List<DynamicFormModel>?> getForms() async {
     try {
-      final response = await _client.from('forms').select();
-      log('VALUES:: ${response.first}');
+      final response = await _client.from(_tableName).select();
 
       final forms = response
           .map(
             (json) => DynamicFormModel.fromJson(json),
           )
-          .toList(); // ← .toList() consumes the iterable
+          .toList();
 
       log('done');
       return forms; // Return the transformed data
