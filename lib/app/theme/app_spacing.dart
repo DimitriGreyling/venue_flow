2005c1aac2d81f8e15ab966d6c2138b2e5cda91e
Dimitri Greyling <67910110+DimitriGreyling@users.noreply.github.com>
2026-09-09 11:05:34 +0200

    remove everything

--- lib/app/theme/app_spacing.dart
+++
@@ -1,7 +0,0 @@
-class AppSpacing {
-  static const double unit = 4;
-  static const double gutter = 24;
-  static const double marginMobile = 16;
-  static const double marginDesktop = 32;
-  static const double containerMaxWidth = 1440;
-}
--- lib/core/error/failures.dart
+++
@@ -1,8 +0,0 @@
-class Failure {
-  final String message;
-  final int? statusCode;
-  const Failure(this.message, {this.statusCode});
-
-  @override
-  String toString() => message;
-}
